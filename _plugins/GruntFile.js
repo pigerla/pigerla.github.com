6fbc013e18a67e7dc58564fe654f67b15e4dd131
pigerla <wjj474957860@gmail.com>
2014-04-05 20:49:19 +0800

    use  combine.min.js

--- _plugins/GruntFile.js
+++ _plugins/GruntFile.js
@@ -14,17 +14,29 @@ module.exports = function (grunt) {
                 ext: '.min.css'
             },
 //合并css
-            combine: {
+            combineCSS: {
                 files: {
                     '../assets/css/combine.min.css': ['../assets/themes/DIYtheme/css/syntax.min.css', '../assets/themes/DIYtheme/css/screen.min.css', '../assets/themes/DIYtheme/css/page.min.css']
+                }
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            combineJS: {
+                files: {
+                    '../assets/js/combine.min.js': ['../assets/js/search-xml.js' , '../assets/js/backToTop.js']
                 }
             }
         }
     });
 
-    //加载“cssmin”插件
+//加载 cssmin 插件
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+//加载 uglify 插件
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // 默认被执行的任务列表。
-    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['cssmin','uglify']);
 };
